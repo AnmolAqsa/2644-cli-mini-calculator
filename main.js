fb8ddd610209a1844f094489dcd1b1589b7641f9
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 14:26:59 -0700

    Add files via upload
    created cli based mini calculator

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const calculator = await inquirer.prompt([
+    { message: "Enter your first number",
+        type: "number",
+        name: "num1",
+    },
+    { message: "Enter your second number",
+        type: "number",
+        name: "num2",
+    },
+    {
+        message: "Select your operator",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+console.log(calculator);
+if (calculator.operators == "Addition") {
+    console.log(calculator.num1 + calculator.num2);
+}
+else if (calculator.operators == "Subtraction") {
+    console.log(calculator.num1 - calculator.num2);
+}
+else if (calculator.operators == "Multiplication") {
+    console.log(calculator.num1 * calculator.num2);
+}
+else if (calculator.operators == "Division") {
+    console.log(calculator.num1 / calculator.num2);
+}
+else {
+    console.log("Seclect valid oprator");
+}
+;
